cb344e78e98d9a0ca27256218579c4ff423d4c88
Paolo <diciaula.paolo@gmail.com>
2022-01-13 00:00:08 +0100

    add custom space with css

--- src/components/Particles/Particles.tsx
+++ src/components/Particles/Particles.tsx
@@ -1,11 +1,14 @@
-import {useEffect} from "react";
+import "./Particles.scss";
+
+function r(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+}
+
 
 export const Particles = () => {
     return <div className='particles'>
         {
-            [...new Array(100)].map(() => {
-                <div className='star' />
-            })
+            [...new Array(100)].map(() => <div style={{animation: `star-light ${r(5, 10)}s ${r(5, 10)}s infinite`, top: (Math.random() * 100)+'%', left: (Math.random() * 100)+'%'}} className='star' />)
         }
     </div>
 }
