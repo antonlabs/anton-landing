04ad261fb658c401718bf68fe5fbc69ce1bc4c19
Mario Agati <agati.mario.93+github@gmail.com>
2022-01-14 19:09:05 +0100

    fix(infrastructure): fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ fi
 
 if [[ "$ENV" == "prod" ]]; then
   aws s3 rm s3://prod.anton.webapp.fe --recursive && aws s3 sync ./build/ s3://prod.anton.webapp.fe --delete
-  aws cloudfront create-invalidation --distribution-id 	E3FP9EIJVIVKNY --paths '/*'
+  aws cloudfront create-invalidation --distribution-id 	E3MBFSLJ4QSSNO --paths '/*'
 fi
