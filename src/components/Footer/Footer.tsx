ee329af25f73ba740b142021e26ff8e3fbb9eaae
Paolo <diciaula.paolo@gmail.com>
2022-01-14 14:40:57 +0100

    fix rel security

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,24 +6,24 @@ export const Footer = (): JSX.Element => {
         <div className={'w-100 flex-row between'}>
             <div className={'flex-column'}>
                 <h3>About Anton</h3>
-                <a target={'_blank'} href={'https://twitter.com/Anton96113792'}>Community</a>
-                <a href={'mailto:contact@anton.finance'}>Contact</a>
+                <a target={'_blank'} rel="noopener" href={'https://twitter.com/Anton96113792'}>Community</a>
+                <a rel="noopener" href={'mailto:contact@anton.finance'}>Contact</a>
             </div>
             <div className={'flex-column'}>
                 <h3>Help me!</h3>
-                <a href={'mailto:support@anton.finance'}>Customer support</a>
+                <a rel="noopener" href={'mailto:support@anton.finance'}>Customer support</a>
             </div>
             <div className={'flex-column'}>
                 <h3>Social</h3>
                 <div className={'flex-row socials'}>
-                    <a target={'_blank'} href={'https://github.com/antonlabs'}><BsGithub /></a>
-                    <a target={'_blank'} href={'https://twitter.com/Anton96113792'}><BsTwitter /></a>
-                    <a target={'_blank'} href={'https://www.instagram.com/anton_copilot/'}><BsInstagram/></a>
+                    <a rel="noopener" target={'_blank'} href={'https://github.com/antonlabs'}><BsGithub /></a>
+                    <a rel="noopener" target={'_blank'} href={'https://twitter.com/Anton96113792'}><BsTwitter /></a>
+                    <a rel="noopener" target={'_blank'} href={'https://www.instagram.com/anton_copilot/'}><BsInstagram/></a>
                 </div>
             </div>
         </div>
         <div className={'credits'}>
-            <a href="https://www.freepik.com/vectors/logo">Logo vector created by catalyststuff - www.freepik.com</a>
+            <a rel="noopener" href="https://www.freepik.com/vectors/logo">Logo vector created by catalyststuff - www.freepik.com</a>
         </div>
     </div>
 }
