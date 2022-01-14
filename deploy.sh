fd026ee26628ceba39f5e1d1b7006c3218b4a9c6
Mario Agati <agati.mario.93+github@gmail.com>
2022-01-14 17:55:15 +0100

    fix(frontend-pipeline): fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,19 @@ if [[ "$ENV" == "" ]]; then
     exit 2
 fi
 
-if [[ "$ENV" != "prod" ]] && [[ "$ENV" != "qa" ]] && [[ "$ENV" != "dev" ]] ]]; then
+if [[ "$ENV" != "prod" ]] && [[ "$ENV" != "qa" ]] && [[ "$ENV" != "dev" ]]; then
     echo "ENV is missing, please use [prod|qa|dev]"
     echo "Usage: $0 <environment-name> [<profile-name>]"
     exit 2
 fi
 
 
-if [[ "$ENV" == "develop" ]] ]]; then
+if [[ "$ENV" == "develop" ]]; then
   aws s3 rm s3://dev.anton.webapp.fe --recursive && aws s3 sync ./build/ s3://dev.anton.webapp.fe --delete
   aws cloudfront create-invalidation --distribution-id E3N1LVA6BXVGS7 --paths '/*'
 fi
 
-if [[ "$ENV" == "prod" ]] ]]; then
+if [[ "$ENV" == "prod" ]]; then
   aws s3 rm s3://dev.anton.webapp.fe --recursive && aws s3 sync ./build/ s3://prod.anton.webapp.fe --delete
   aws cloudfront create-invalidation --distribution-id E3N1LVA6BXVGS7 --paths '/*'
 fi
